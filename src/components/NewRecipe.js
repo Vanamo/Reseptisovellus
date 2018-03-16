bbf7150d772060a34a41c2a1e503a628eb4760ff
Piirainen Vanamo K <vseppane@da3-cs-dk110-08.pc.helsinki.fi>
2018-03-16 17:28:20 +0200

    reseptin luonti, ei valmis

--- src/components/NewRecipe.js
+++ src/components/NewRecipe.js
@@ -1,19 +1,72 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { newRecipe } from './../reducers/recipeReducer'
+import { newSuccessNotification } from './../reducers/notificationReducer'
 import { Form, Button } from 'semantic-ui-react'
 
 class NewRecipe extends React.Component {
 
-  state = {}
+  state = {
+    title: '',
+    ingredients: [{ quantity: '', unit: '', name: '' }],
+    instructions: '',
+    tags: []
+  }
 
   options = [
     { key: 'j', text: 'laktoositon', value: 'laktoositon' },
     { key: 'm', text: 'vegaaninen', value: 'vegaaninen' }
   ]
 
-  onSubmit = (event) => {
+  handleAddIngredient = () => {
+    this.setState({
+      ingredients: this.state.ingredients
+        .concat([{ quantity: '', unit: '', name: '' }])
+    })
+  }
+
+  handleRemoveIngredient = (idx) => () => {
+    this.setState({
+      ingredients: this.state.ingredients.filter((ing, ind) => ind !== idx)
+    })
+  }
+
+  handleIngredientChange = (idx) => (event) => {
+    const newIngredients = this.state.ingredients.map((ingredient, ind) => {
+      if (ind !== idx) return ingredient
+      return { ...ingredient, [event.target.name]: event.target.value }
+    })
+    this.setState({ ingredients: newIngredients })
+  }
+
+  handleFieldChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  handleTagChange = (event) => {
+    console.log('e', event.target.value)
+
+  }
+
+  onSubmit = async (event) => {
     event.preventDefault()
+    const recipeObject = {
+      title: this.state.title,
+      ingredients: this.state.ingredients,
+      instructions: this.state.instructions,
+      tags: this.state.tags
+    }
+
+    await this.props.newRecipe(recipeObject)
+
+    this.props.newSuccessNotification('Uusi resepti luotu', 5)
+
+    this.setState({
+      title: '',
+      ingredients: [{ quantity: '', unit: '', name: '' }],
+      instructions: '',
+      tags: []
+    })
   }
 
   render() {
@@ -21,17 +74,69 @@ class NewRecipe extends React.Component {
       <div>
         <h2>Lisää uusi resepti</h2>
         <Form onSubmit={this.onSubmit}>
-          <Form.Input label='Reseptin nimi' />
-          <Form.Select
-            label='Raaka-aineet'
-            options={this.options} placeholder=''
+          <Form.Input
+            label='Reseptin nimi'
+            name='title'
+            value={this.state.title}
+            onChange={this.handleFieldChange}
           />
-          <Form.TextArea label='Valmistusohje' />
+          <strong>Raaka-aineet</strong>
+          <p></p>
+          {this.state.ingredients.map((ingredient, idx) => (
+            <Form.Group widths='equal' key={idx}>
+              <Form.Input
+                label='määrä'
+                name='quantity'
+                type='number'
+                min='0'
+                step='any'
+                value={ingredient.quantity}
+                onChange={this.handleIngredientChange(idx)}
+              />
+              <Form.Select
+                label='yksikkö'
+                name='unit'
+                options={this.options}
+                placeholder=''
+                value={ingredient.unit}
+                onChange={this.handleIngredientChange(idx)}
+              />
+              <Form.Select
+                label='raaka-aine'
+                name='name'
+                options={this.options}
+                placeholder=''
+                value={ingredient.name}
+                onChange={this.handleIngredientChange(idx)}
+              />
+              <Button
+                negative
+                onClick={this.handleRemoveIngredient(idx)}>
+                Poista
+              </Button>
+            </Form.Group>
+          ))}
+          <Button
+            type='button'
+            onClick={this.handleAddIngredient}>
+            Uusi raaka-aine
+          </Button>
+          <p></p>
+          <Form.TextArea
+            label='Valmistusohje'
+            name='instructions'
+            value={this.state.instructions}
+            onChange={this.handleFieldChange}
+          />
           <Form.Dropdown
             label='Tagit'
-            fluid multiple selection options={this.options} placeholder=''
+            fluid multiple selection options={this.options}
+            placeholder=''
+            name='tags'
+            value={this.state.tags}
+            onChange={this.handleTagChange}
           />
-          <Form.Button>Luo resepti</Form.Button>
+          <Form.Button positive>Luo resepti</Form.Button>
         </Form>
       </div>
     )
@@ -40,5 +145,5 @@ class NewRecipe extends React.Component {
 
 export default connect(
   null,
-  { newRecipe }
+  { newRecipe, newSuccessNotification }
 )(NewRecipe)
